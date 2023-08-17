2bd2f11b00247709348f4f452dde4de0b8983abc
Giacomo Di Valerio <giacomo.divalerio@par-tec.it>
2023-08-17 09:53:48 +0200

    env

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,10 @@
-'''
-Created on 04-Sep-2019
-
-@author: bkadambi
-'''
-
-# -*- coding: UTF-8 -*-
-"""
-hello_flask: First Python-Flask webapp
-"""
+import os
 
 def main():
-    """Say hello"""
     return 'Hello, world!'
 
-if __name__ == '__main__':  # Script executed directly?
+if __name__ == '__main__':  
     print("Hello World! Built with a Docker file.")
-    
+    mylist = os.environ.get('MY_LIST')
+    print(mylist)
+    print("mylist:", type(mylist))
