50a4ea4235978d5bf1fc64c2fa1c0fc08a955ffa
Giacomo Di Valerio <giacomo.divalerio@par-tec.it>
2023-08-17 10:12:18 +0200

    listCommand

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,7 @@ def main():
 
 if __name__ == '__main__':  
     print("Hello World! Built with a Docker file.")
-    mylist = os.environ.get('MY_LIST')
-    print(mylist)
-    print("mylist:", type(mylist))
+    mylist_string = os.environ.get('MY_LIST')
+    listCommand = mylist_string.split(";")
+    print(listCommand)
+    print("mylist:", type(listCommand))
