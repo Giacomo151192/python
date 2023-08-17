8e15da8c61c7a3fba8c046074dd7f51299586415
Giacomo Di Valerio <giacomo.divalerio@par-tec.it>
2023-08-17 11:30:11 +0200

    dockerfile modifiche2

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM registry.access.redhat.com/ubi8/python-39
 
 WORKDIR /app
 COPY . /app
-
+RUN pip install --upgrade pip \
+    && pip install -r requirements.txt
 EXPOSE 5000
 
 ENTRYPOINT ["python"]
--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,22 @@
 import os
+from decouple import config
 
 def main():
     return 'Hello, world!'
 
 if __name__ == '__main__':  
-    print("Hello World! Built with a Docker file.")
-    mylist_string = os.environ.get('MY_LIST')
+    # os.environ['MY_LIST'] = "value1,value2;value3,value4;\n"
+    
+    listCommand = config("MY_LIST", cast = lambda v : [s.split(",") for s in v.split(";") if not s.startswith("\n")])
+    
+    print(listCommand[0][0])
+    
+    # mylist_string = os.environ.get('MY_LIST', "value1,value2;value3,value4;\n")
 
-    listCommand = mylist_string.split(";", "\n")
-    print(listCommand)
-    print("mylist:", type(listCommand))
+    # listCommand = mylist_string.split(";")
+    # listCommand.pop()
+    # listSingleCommand = []
+    # for item in listCommand:
+    #     listSingleCommand.extend(item.split(","))
+    # print(listSingleCommand)
+    # print("mylist:", type(listSingleCommand))
